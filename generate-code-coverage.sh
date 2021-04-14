f038542759174aad4c6e4e8a0d1ecbba7b197c64
Jonathan Johnson <jon@khonsulabs.com>
2021-04-14 09:55:53 -0700

    Fixing cargo-badge check

--- generate-code-coverage.sh
+++ generate-code-coverage.sh
@@ -3,8 +3,9 @@ echo "Executing tests"
 CARGO_INCREMENTAL=0 LLVM_PROFILE_FILE="%m.profraw" RUSTFLAGS="-Zinstrument-coverage" RUSTDOCFLAGS="-Cpanic=abort" cargo +nightly test --all-features
 echo "Generating coverage report"
 grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing --llvm -o coverage/
-if ! command -v cargo-badge &> /dev/null
+if command -v cargo-badge &> /dev/null
 then
+    echo "Generating badge"
     cargo badge -s "coverage" "`cat coverage/index.html | grep -oP '\d+(\.\d+)? %' | head -n1`" -o coverage/badge.svg
 fi
 echo "Cleaning up."
