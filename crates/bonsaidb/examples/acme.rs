4141eaa8b2201a7735b7ce9e6a9c987fc25449b3
Jonathan Johnson <jon@khonsulabs.com>
2021-11-14 17:01:58 -0800

    Comment Acme example

--- crates/bonsaidb/examples/acme.rs
+++ crates/bonsaidb/examples/acme.rs
@@ -1,121 +1,67 @@
-//! Shows basic usage of a server.
+//! Shows how to use ACME to automatically acquire a TLS certificate for your `BonsaiDb` server.
 
 use std::{path::Path, time::Duration};
 
 use bonsaidb::{
     client::{url::Url, Client},
-    core::{
-        connection::{Connection, ServerConnection},
-        schema::Collection,
-    },
+    core::connection::ServerConnection,
     server::{Configuration, DefaultPermissions, Server},
 };
 use bonsaidb_server::{AcmeConfiguration, LETS_ENCRYPT_STAGING_DIRECTORY};
-use rand::{thread_rng, Rng};
 
-mod support;
-use support::schema::{Shape, ShapesByNumberOfSides};
+const DOMAIN: &str = "example.com";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // ANCHOR: setup
     let server = Server::open(
         Path::new("acme-server-data.bonsaidb"),
         Configuration {
+            server_name: String::from(DOMAIN),
             default_permissions: DefaultPermissions::AllowAll,
             acme: AcmeConfiguration {
-                primary_domain: String::from("dev.ncog.id"),
-                contact_email: None,
+                contact_email: Some(String::from("mailto:netops@example.com")),
                 directory: LETS_ENCRYPT_STAGING_DIRECTORY.to_string(),
             },
             ..Default::default()
         },
     )
     .await?;
+    server.register_schema::<()>().await?;
 
-    server.register_schema::<Shape>().await?;
-    server.create_database::<Shape>("my-database", true).await?;
-    // ANCHOR_END: setup
-
-    // If websockets are enabled, we'll also listen for websocket traffic. The
-    // QUIC-based connection should be overall better to use than WebSockets,
-    // but it's much easier to route WebSocket traffic across the internet.
+    // The ACME registration is done via the TLS-ALPN-01 challenge, which occurs
+    // on port 443 for LetsEncrypt. With the feature enabled, listening for
+    // HTTPS traffic will automatically.
     let task_server = server.clone();
     tokio::spawn(async move {
-        task_server.listen_for_https_on("0.0.0.0:5001").await
+        task_server.listen_for_https_on("0.0.0.0:443").await
     });
 
-    // ugly, wait for the certificate
+    // Once the ACME process has succeded, the certificate_chain will be able to
+    // be retrieved.
     while server.certificate_chain().await.is_err() {
         tokio::time::sleep(Duration::from_secs(1)).await;
     }
 
-    // Spawn our QUIC-based protocol listener.
+    // Spawn our QUIC-based protocol listener. This will use the same
+    // certificate as the HTTPS port.
     let task_server = server.clone();
     tokio::spawn(async move { task_server.listen_on(5645).await });
 
-    // Give a moment for the listeners to start.
+    // Give a moment for the QUIC listener to start.
     tokio::time::sleep(Duration::from_millis(10)).await;
 
-    // To allow this example to run both websockets and QUIC, we're going to gather the clients
-    // into a collection and use join_all to wait until they finish.
-    let mut tasks = Vec::new();
-    // To connect over websockets, use the websocket scheme.
-    tasks.push(do_some_database_work(
-        Client::new(Url::parse("wss://dev.ncog.id")?)
-            .await?
-            .database::<Shape>("my-database")
-            .await?,
-        "websockets",
-    ));
+    // Test connecting using both clients.
+    let client =
+        Client::new(Url::parse(&format!("bonsaidb://{}", DOMAIN))?).await?;
+    client.create_database::<()>("test-database", true).await?;
 
-    // To connect over QUIC, use the bonsaidb scheme.
-    tasks.push(do_some_database_work(
-        Client::<()>::build(Url::parse("bonsaidb://dev.ncog.id")?)
-            .finish()
-            .await?
-            .database::<Shape>("my-database")
-            .await?,
-        "bonsaidb",
-    ));
-
-    // Wait for the clients to finish
-    futures::future::join_all(tasks)
-        .await
-        .into_iter()
-        .collect::<anyhow::Result<()>>()?;
-
-    // Shut the server down gracefully (or forcefully after 5 seconds).
-    server.shutdown(Some(Duration::from_secs(5))).await?;
-
-    Ok(())
-}
-
-async fn do_some_database_work<'a, C: Connection>(
-    database: C,
-    client_name: &str,
-) -> anyhow::Result<()> {
-    // Insert 50 random shapes
-    for _ in 0u32..50 {
-        let sides = {
-            let mut rng = thread_rng();
-            rng.gen_range(3..=10)
-        };
-        Shape::new(sides).insert_into(&database).await?;
-    }
-
-    println!("Client {} finished", client_name);
-
-    // Print a summary of all the shapes
-    for result in database
-        .view::<ShapesByNumberOfSides>()
-        .reduce_grouped()
-        .await?
+    #[cfg(feature = "websockets")]
     {
-        println!(
-            "Number of entries with {:02} sides: {}",
-            result.key, result.value
-        );
+        let websockets =
+            Client::new(Url::parse(&format!("wss://{}", DOMAIN))?).await?;
+        websockets
+            .create_database::<()>("test-database", true)
+            .await?;
     }
 
     Ok(())
