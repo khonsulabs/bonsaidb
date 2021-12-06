05696281651d22aa319b859ad13452646d1cfc8e
Jonathan Johnson <jon@khonsulabs.com>
2021-12-06 09:09:27 -0800

    Switched keystorage to the official aws sdk
    This changes how the custom endpoints are handled, and makes things like
    how the credentials are passed in be standardized the same way as all
    AWS sdk implementations.

--- crates/bonsaidb-keystorage-s3/src/lib.rs
+++ crates/bonsaidb-keystorage-s3/src/lib.rs
@@ -11,31 +11,22 @@
 //! To use this, specify the `vault_key_storage` configuration parameter:
 //!
 //! ```rust
-//! # use bonsaidb_core::{
-//! # connection::StorageConnection,
-//! # document::KeyId,
-//! # schema::Collection,
-//! # test_util::{Basic, BasicSchema, TestDirectory},
-//! # };
-//! # use bonsaidb_local::{config::Configuration, Storage};
 //! # use bonsaidb_keystorage_s3::S3VaultKeyStorage;
+//! # use aws_sdk_s3::Endpoint;
+//! # use bonsaidb_core::{document::KeyId, test_util::TestDirectory};
+//! # use bonsaidb_local::config::Configuration;
+//! # use http::Uri;
 //! #
-//! # let directory = TestDirectory::new("bonsaidb-keystorage-s3-basic");
-//! # let configuration = Configuration {
-//! vault_key_storage: Some(Box::new(S3VaultKeyStorage::from(
-//!         s3::Bucket::new(
-//!             "bucket-name",
-//!             s3::Region::Custom {
-//!                 endpoint: String::from("s3.us-west-001.backblazeb2.com"),
-//!                 region: String::default(),
-//!             },
-//!             s3::creds::Credentials::new(Some(&"access key id"), Some(&"secret access key"), None, None, None).unwrap(),
-//!         )
-//!         .unwrap(),
+//! let directory = TestDirectory::new("bonsaidb-keystorage-s3-basic");
+//! let configuration = Configuration {
+//!     vault_key_storage: Some(Box::new(S3VaultKeyStorage::new("bucket_name").endpoint(
+//!         Endpoint::immutable(
+//!             Uri::try_from("https://s3.us-west-001.backblazeb2.com").unwrap(),
+//!         ),
 //!     ))),
-//! # default_encryption_key: Some(KeyId::Master),
-//! # ..Configuration::default()
-//! # };
+//!     default_encryption_key: Some(KeyId::Master),
+//!     ..Configuration::default()
+//! };
 //! ```
 //!
 //! The API calls are performed by the [`s3`] crate.
@@ -61,34 +52,49 @@
 use std::fmt::Display;
 
 use async_trait::async_trait;
+use aws_config::meta::region::RegionProviderChain;
+pub use aws_sdk_s3;
+use aws_sdk_s3::{error::GetObjectErrorKind, Client, Endpoint, Region};
 use bonsaidb_local::{
     vault::{PrivateKey, VaultKeyStorage},
     StorageId,
 };
-pub use s3;
-use s3::Bucket;
+pub use http;
 
 /// S3-compatible [`VaultKeyStorage`] implementor.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct S3VaultKeyStorage {
-    bucket: Bucket,
+    bucket: String,
+    /// The S3 endpoint to use. If not specified, the endpoint will be
+    /// determined automatically. This field can be used to support non-AWS S3
+    /// providers.
+    pub endpoint: Option<Endpoint>,
+    /// The AWS region to use. If not specified, the region will be determined
+    /// by the aws sdk.
+    pub region: Option<Region>,
     /// The path prefix for keys to be stored within.
     pub path: String,
 }
 
-impl From<Bucket> for S3VaultKeyStorage {
-    fn from(bucket: Bucket) -> Self {
+impl S3VaultKeyStorage {
+    /// Creates a new key storage instance for `bucket`.
+    pub fn new(bucket: impl Display) -> Self {
         Self {
-            bucket,
-            path: String::from(""),
+            bucket: bucket.to_string(),
+            ..Self::default()
         }
     }
-}
 
-impl S3VaultKeyStorage {
     /// Sets the path prefix for vault keys to be stored within.
     pub fn path(mut self, prefix: impl Display) -> Self {
         self.path = prefix.to_string();
+        self
+    }
+
+    /// Sets the endpoint to use. See [`Self::endpoint`] for more information.
+    #[allow(clippy::missing_const_for_fn)] // destructors
+    pub fn endpoint(mut self, endpoint: Endpoint) -> Self {
+        self.endpoint = Some(endpoint);
         self
     }
 
@@ -99,6 +105,25 @@ impl S3VaultKeyStorage {
         }
         path.push_str(&storage_id.to_string());
         path
+    }
+
+    async fn client(&self) -> aws_sdk_s3::Client {
+        let region_provider = RegionProviderChain::first_try(self.region.clone())
+            .or_default_provider()
+            .or_else(Region::new("us-east-1"));
+        let config = aws_config::from_env().load().await;
+        if let Some(endpoint) = self.endpoint.clone() {
+            Client::with_config(
+                aws_smithy_client::Client::https(),
+                aws_sdk_s3::Config::builder()
+                    .endpoint_resolver(endpoint)
+                    .region(region_provider.region().await)
+                    .credentials_provider(config.credentials_provider().unwrap().clone())
+                    .build(),
+            )
+        } else {
+            aws_sdk_s3::Client::new(&config)
+        }
     }
 }
 
@@ -111,30 +136,45 @@ impl VaultKeyStorage for S3VaultKeyStorage {
         storage_id: StorageId,
         key: PrivateKey,
     ) -> Result<(), Self::Error> {
-        let (_bytes, code) = self
-            .bucket
-            .put_object(&self.path_for_id(storage_id), &key.to_bytes()?)
+        let client = self.client().await;
+        let key = key.to_bytes()?;
+        client
+            .put_object()
+            .bucket(&self.bucket)
+            .key(self.path_for_id(storage_id))
+            .body(aws_sdk_s3::ByteStream::from(key.to_vec()))
+            .send()
             .await?;
-        if (200..300).contains(&code) {
-            Ok(())
-        } else {
-            anyhow::bail!("error uploading to s3 bucket: {}", code)
-        }
+        Ok(())
     }
 
     async fn vault_key_for(
         &self,
         storage_id: StorageId,
     ) -> Result<Option<PrivateKey>, Self::Error> {
-        let (bytes, code) = self.bucket.get_object(self.path_for_id(storage_id)).await?;
-        if (200..300).contains(&code) {
-            let key =
-                PrivateKey::from_bytes(&bytes).map_err(|err| anyhow::anyhow!(err.to_string()))?;
-            Ok(Some(key))
-        } else if code == 404 {
-            Ok(None)
-        } else {
-            anyhow::bail!("unexpected status code from s3 bucket: {}", code)
+        let client = self.client().await;
+        match client
+            .get_object()
+            .bucket(&self.bucket)
+            .key(self.path_for_id(storage_id))
+            .send()
+            .await
+        {
+            Ok(response) => {
+                let bytes = response.body.collect().await?.into_bytes();
+                let key = PrivateKey::from_bytes(&bytes)
+                    .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+                Ok(Some(key))
+            }
+            Err(aws_sdk_s3::SdkError::ServiceError {
+                err:
+                    aws_sdk_s3::error::GetObjectError {
+                        kind: GetObjectErrorKind::NoSuchKey(_),
+                        ..
+                    },
+                ..
+            }) => Ok(None),
+            Err(err) => Err(anyhow::anyhow!(err)),
         }
     }
 }
@@ -149,6 +189,7 @@ async fn basic_test() {
         test_util::{Basic, BasicSchema, TestDirectory},
     };
     use bonsaidb_local::{config::Configuration, Storage};
+    use http::Uri;
     drop(dotenv::dotenv());
 
     macro_rules! env_var {
@@ -166,26 +207,17 @@ async fn basic_test() {
         }};
     }
 
-    let key_id = env_var!("S3_ACCESS_KEY_ID");
-    let key_secret = env_var!("S3_ACCESS_KEY_SECRET");
-    let bucket_name = env_var!("S3_BUCKET");
+    let bucket = env_var!("S3_BUCKET");
     let endpoint = env_var!("S3_ENDPOINT");
 
     let directory = TestDirectory::new("bonsaidb-keystorage-s3-basic");
 
     let configuration = |prefix| {
-        let mut vault_key_storage = S3VaultKeyStorage::from(
-            Bucket::new(
-                &bucket_name,
-                s3::Region::Custom {
-                    endpoint: endpoint.clone(),
-                    region: String::default(),
-                },
-                s3::creds::Credentials::new(Some(&key_id), Some(&key_secret), None, None, None)
-                    .unwrap(),
-            )
-            .unwrap(),
-        );
+        let mut vault_key_storage = S3VaultKeyStorage {
+            bucket: bucket.clone(),
+            endpoint: Some(Endpoint::immutable(Uri::try_from(&endpoint).unwrap())),
+            ..S3VaultKeyStorage::default()
+        };
         if let Some(prefix) = prefix {
             vault_key_storage = vault_key_storage.path(prefix);
         }
