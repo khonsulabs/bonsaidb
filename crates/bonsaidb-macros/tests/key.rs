dc846989d6ab1ce452e7fc104884285c5ae2144a
Roland Fredenhagen <dev@modprog.de>
2022-04-09 18:29:28 +0200

    add tests

--- crates/bonsaidb-macros/tests/key.rs
+++ crates/bonsaidb-macros/tests/key.rs
@@ -1,9 +1,17 @@
-use bonsaidb::core::key::Key;
+use bonsaidb::core::key::{Key, KeyEncoding};
 
 #[test]
 fn tuple_struct() {
     #[derive(Clone, Debug, Key)]
     struct Test(i32, i32, String);
+
+    assert_eq!(
+        &[0, 0, 0, 1, 0, 0, 0, 2, 116, 101, 115, 116, 0, 4],
+        Test(1, 2, "test".into())
+            .as_ord_bytes()
+            .unwrap()
+            .as_ref()
+    )
 }
 
 #[test]
@@ -13,6 +21,16 @@ fn struct_struct() {
         a: i32,
         b: String,
     }
+    assert_eq!(
+        &[255, 255, 255, 214, 109, 101, 97, 110, 105, 110, 103, 0, 7],
+        Test {
+            a: -42,
+            b: "meaning".into()
+        }
+        .as_ord_bytes()
+        .unwrap()
+        .as_ref()
+    )
 }
 
 #[test]
@@ -23,4 +41,19 @@ fn r#enum() {
         B(i32, String),
         C { a: String, b: i32 },
     }
+
+    assert_eq!(
+        &[0, 0, 0, 0, 0, 0, 0, 0],
+        Test::A.as_ord_bytes().unwrap().as_ref()
+    );
+
+    assert_eq!(
+        &[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 97, 0, 1],
+        Test::B(2, "a".into()).as_ord_bytes().unwrap().as_ref()
+    );
+
+    assert_eq!(
+        &[0, 0, 0, 0, 0, 0, 0, 2, 98, 0, 0, 0, 0, 3, 1],
+        Test::C{a: "b".into(), b: 3}.as_ord_bytes().unwrap().as_ref()
+    )
 }
