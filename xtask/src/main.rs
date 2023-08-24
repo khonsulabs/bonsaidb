a3e412c2e94434eed4b85350c30cb0779fdf38c7
Jonathan Johnson <jon@khonsulabs.com>
2023-08-24 13:40:14 -0700

    Updating msrv for test matrix

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -93,7 +93,7 @@ fn all_tests() -> &'static [TestSuite] {
         },
         TestSuite {
             cargo_args: "--all-features",
-            toolchain: "1.65",
+            toolchain: "1.70",
         },
         TestSuite {
             cargo_args: "--package bonsaidb-core --no-default-features",
