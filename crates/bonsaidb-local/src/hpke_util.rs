fa407ce9ae4951618137006aa1817fb479b8ff13
Michael Rosenberg <michael@mrosenberg.pub>
2023-11-18 13:46:30 -0500

    Simplified HPKE serde impls

--- crates/bonsaidb-local/src/hpke_util.rs
+++ crates/bonsaidb-local/src/hpke_util.rs
@@ -20,69 +20,32 @@ pub(crate) type VaultP256EncappedKey = <DhP256HkdfSha256 as KemTrait>::EncappedK
 // We put everything in its own module so we can use the `with` field attribute
 // https://serde.rs/field-attrs.html#with
 
-pub(crate) mod serde_pubkey {
-    use super::*;
+// Impl serde for $t: hpke::{Serializable, Deserializable}
+macro_rules! impl_serde {
+    ($modname:ident, $t:ty) => {
+        pub(crate) mod $modname {
+            use super::*;
 
-    pub(crate) fn serialize<S: Serializer>(
-        public_key: &VaultP256PublicKey,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error> {
-        let arr = public_key.to_bytes();
-        arr.serialize(serializer)
-    }
+            pub(crate) fn serialize<S: Serializer>(
+                val: &$t,
+                serializer: S,
+            ) -> Result<S::Ok, S::Error> {
+                let arr = val.to_bytes();
+                arr.serialize(serializer)
+            }
 
-    pub(crate) fn deserialize<'de, D: Deserializer<'de>>(
-        deserializer: D,
-    ) -> Result<VaultP256PublicKey, D::Error> {
-        let arr =
-            GenericArray::<u8, <VaultP256PublicKey as Serializable>::OutputSize>::deserialize(
-                deserializer,
-            )?;
-        VaultP256PublicKey::from_bytes(&arr).map_err(D::Error::custom)
-    }
+            pub(crate) fn deserialize<'de, D: Deserializer<'de>>(
+                deserializer: D,
+            ) -> Result<$t, D::Error> {
+                let arr = GenericArray::<u8, <$t as Serializable>::OutputSize>::deserialize(
+                    deserializer,
+                )?;
+                <$t>::from_bytes(&arr).map_err(D::Error::custom)
+            }
+        }
+    };
 }
 
-pub(crate) mod serde_privkey {
-    use super::*;
-    use hpke::Serializable;
-
-    pub(crate) fn serialize<S: Serializer>(
-        private_key: &VaultP256PrivateKey,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error> {
-        let arr = private_key.to_bytes();
-        arr.serialize(serializer)
-    }
-
-    pub(crate) fn deserialize<'de, D: Deserializer<'de>>(
-        deserializer: D,
-    ) -> Result<VaultP256PrivateKey, D::Error> {
-        let arr =
-            GenericArray::<u8, <VaultP256PrivateKey as Serializable>::OutputSize>::deserialize(
-                deserializer,
-            )?;
-        VaultP256PrivateKey::from_bytes(&arr).map_err(D::Error::custom)
-    }
-}
-
-pub(crate) mod serde_encapped_key {
-    use super::*;
-
-    pub(crate) fn serialize<S: Serializer>(
-        encapped_key: &VaultP256EncappedKey,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error> {
-        let arr = encapped_key.to_bytes();
-        arr.serialize(serializer)
-    }
-
-    pub(crate) fn deserialize<'de, D: Deserializer<'de>>(
-        deserializer: D,
-    ) -> Result<VaultP256EncappedKey, D::Error> {
-        let arr =
-            GenericArray::<u8, <VaultP256EncappedKey as Serializable>::OutputSize>::deserialize(
-                deserializer,
-            )?;
-        VaultP256EncappedKey::from_bytes(&arr).map_err(D::Error::custom)
-    }
-}
+impl_serde!(serde_pubkey, VaultP256PublicKey);
+impl_serde!(serde_privkey, VaultP256PrivateKey);
+impl_serde!(serde_encapped_key, VaultP256EncappedKey);
